c843cff761c056880e2fc544e740edf99cf326e2
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2019-01-13 20:56:51 -0200

    mudando pdf

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
 require '../vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
 
 // $query = new RS\DB\Query;
 
@@ -11,5 +17,52 @@ require '../vendor/autoload.php';
 // var_dump($home->index());
 
 
-var_dump(getTrim("asd asdas  asdasd   asd      "));
+//var_dump(getTrim("asd asdas  asdasd   asd      "));
 
+// $obj = new Myclass();
+
+
+// $obj->mymethod();
+
+
+// create a log channel
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// // add records to the log
+// $log->warning('Foo');
+// $log->error('Bar');
+
+
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+
+$html = "<table border='1' width='100%' style='border-collapse: collapse;'>
+        <tr>
+            <th>Username</th><th>Email</th>
+        </tr>
+        <tr>
+            <td>yssyogesh</td>
+            <td>yogesh@makitweb.com</td>
+        </tr>
+        <tr>
+            <td>sonarika</td>
+            <td>sonarika@gmail.com</td>
+        </tr>
+        <tr>
+            <td>vishal</td>
+            <td>vishal@gmail.com</td>
+        </tr>
+        </table>";
+        
+$dompdf->loadHtml($html);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
